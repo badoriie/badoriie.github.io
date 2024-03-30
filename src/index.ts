adafd67b6f8b3e814b9f97d4098dfd15cf8ee8d0
Amir B. Nasari <me@badoriie.com>
2024-03-30 23:11:10 +0100

    chore(app): Remove dist files and add docs files #Bado-1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+// Correct usage
+document.addEventListener('DOMContentLoaded', function() {
+  // Ensure DOM is fully loaded before accessing elements
+  const element = document.getElementById('demo');
+  if (element !== null) {
+      // Check if element exists before setting its innerHTML
+      element.innerHTML = "This website is under construction! stay tuned for updates!";
+  } else {
+      console.error('Element does not exist');
+  }
+});
